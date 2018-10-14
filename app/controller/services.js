77301d2c30a02dd989ca0da0bfb711bfad1dab0c
Sairam <sai.chakka@intellectdesign.com>
2018-10-14 16:00:40 +0530

    first commit

--- app/controller/services.js
+++ app/controller/services.js
@@ -1,3 +1,12 @@
+webPush = require('web-push');
+
+webPush.setVapidDetails(
+    'mailto:hallo@justmarkup.com',
+    "BM57AFDte_mQMHPlncj_w5bsl_sp6JFCp5cP8qF2e6BdMKmAmyrElprAAaF53dP4qvWaiGF3oUJ1U48qRD_5duA", // process.env.VAPID_PUBLIC_KEY,
+    "BhesDzt6VxlybAf2XYtMr0dFsrW0BVT9Qznzqy29TDA" // process.env.VAPID_PRIVATE_KEY
+);
+
+
 module.exports = {
     cfrData: function(req, res, callback) {
         console.log('cfr');
@@ -15,76 +24,113 @@ module.exports = {
             console.log(msg);
             console.log(clients.length);
             clients.forEach(function(client, i) {
-                if(client.connection!=ws)
-                client.connection.send(msg);
+                if (client.connection != ws) {
+                    client.connection.send(msg);
+                    
+                }
+             
+                subscriptions.forEach(function(subscription) {
+                      
+                        pushMessage(subscription.subscription, 'Details have been received successfully');
+                     
+                        
+                    })
             });
         });
         ws.on('close', function(msg) {
             console.log(msg);
-            
-           clients.splice(clients.indexOf(ws),1);
-           ws.close();
-           
-           console.log(clients.length);
+
+            clients.splice(clients.indexOf(ws), 1);
+            ws.close();
+
+            console.log(clients.length);
+            subscriptions.forEach(function(subscription) {
+
+                pushMessage(subscription, 'Connection have been closed');
+
+            })
+
         });
 
     },
-    camDetails:function(req,res){
+    camDetails: function(req, res) {
         res.send({
             "address1": "ss",
-"address2":"ss",
-"address3":"ss",
-"addressQuestion":"N",
-"businessAddress":"dd",
-"correspondenceAddress":"dd",
-"country":"india",
-"county":"india",
-"name":"sairam",
-"postcode":"603103",
-"town":"vja",
-"date":"06-09-94"
+            "address2": "ss",
+            "address3": "ss",
+            "addressQuestion": "N",
+            "businessAddress": "dd",
+            "correspondenceAddress": "dd",
+            "country": "india",
+            "county": "india",
+            "name": "sairam",
+            "postcode": "603103",
+            "town": "vja",
+            "date": "06-09-94"
         })
+    },
+
+    pushSubscribe: function(req, res) {
+        var sid = sub++;
+
+        subscriptions.push({
+            id: sid,
+            subscription: {
+                endpoint: req.body.endpoint,
+                keys: {
+                    p256dh: req.body.keys.p256dh,
+                    auth: req.body.keys.auth
+                }
+            }
+        })
+        res.send('success')
+
+
+
+
+    },
+    pushUnsubscribe: function(req, res) {
+
+        // remove from database
+        Push.findOneAndRemove({
+            endpoint: endpoint
+        }, function(err, data) {
+            if (err) {
+                console.error('error with unsubscribe', error);
+                res.status(500).send('unsubscription not possible');
+            }
+            console.log('unsubscribed');
+            res.status(200).send('unsubscribe');
+        });
+
     }
 }
 
 
 
 var count = 0;
+var sub = 0;
 var clients = [];
-// wsServer = new WebSocketServer({
-//     httpServer: server,
-//     path:"/details"
-// }); 
-// wsServer.on('request', function(r){
+var subscriptions = [];
+
+function pushMessage(subscription, body) {
+    webPush.sendNotification(
+        subscription,
+        JSON.stringify({
+            title: 'CFR',
+            body: body,
+
+        }),
+        3600
+    ).then(function() {
+        console.log("Send welcome push notification");
+
+        return;
+    }).catch(err => {
+        console.error("Unable to send welcome push notification", err);
+
+        return;
+    });
+}
 
 
-//     var connection = r.accept('echo-protocol', r.origin);
-//     connection.origin=r.origin
-
-//     
-
-
-
-//     sockConn(connection,id,function(connection,id){
-//         var me =this;
-//         clients.push({'id':id,'conn':connection,'origin':connection.origin});
-
-//                     clients.forEach(function(client,i){
-//                        clients[i].conn.on('message',function(message){
-//                            console.log(message);
-//                             clients.forEach(function(client,i){
-//                                 client.conn.sendUTF(message.utf8Data)
-//                             })
-
-//                     });
-//                    });
-
-// });
-// });
-
-
-
-// function sockConn(connection,id,next){
-//     console.log((new Date()) + ' Connection accepted [' + id + ']');
-//     next(connection,id);
-// }
--- routes.js
+++ routes.js
@@ -5,7 +5,13 @@ router.route('/getData')
     .get(services.cfrData)
 router.post('/camera', services.camDetails);
 router.ws('/details', services.socketConn);
-    
+
+
+
+
+router.post('/push/subscribe', services.pushSubscribe);
+
+router.post('/push/unsubscribe', services.pushUnsubscribe);    
 
 
 
